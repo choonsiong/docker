6187723a8260ebbdc06b85e7d0a471c04a35a321
choonsiong <choonsiong.lee@gmail.com>
2024-11-18 02:07:19 +0800

    Auto commit

---
+++ sap-press-docker/apache-php-helloworld/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>Hello World: apache/php</h1>
+    <?php
+    $load = sys_getloadavg();
+    ?>
+    Date: <?php echo date("c"); ?><br/>
+    Server utilization (load): <?php echo $load[0]; ?>
+</body>
+</html>
